4aa77adadc8956ffb56d27a35a7e74b83a82c63f
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-12 14:01:40 -0700

    edited spider to iterate over selectors and add quotes to item

--- dalecooper_scrapy/dalecooper_scrapy/spiders/dalecooper_spider.py
+++ dalecooper_scrapy/dalecooper_scrapy/spiders/dalecooper_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from dalecooper_scrapy.items import DalecooperScrapyItem
+from scrapy.selector import Selector
 
 class DalecooperSpider(scrapy.Spider):
     name = 'dalecooper'
@@ -7,7 +8,8 @@ class DalecooperSpider(scrapy.Spider):
     start_urls = ['http://www.imdb.com/character/ch0009681/quotes']
 
     def parse(self, response):
-        for sel in response.xpath('//i/a'):
+        sel = Selector(response)
+        for quote in sel.xpath('//i[a[@href="/name/nm0001492/"]]/following-sibling::text()[1]').extract():
             item = DalecooperScrapyItem()
-            item['quote'] = sel.xpath('//i[a[@href="/name/nm0001492/"]]/following-sibling::text()[1]').extract()
+            item['quote'] = quote
             yield item
