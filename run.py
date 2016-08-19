ceacf99624f9b528e853464376c4dd5b400b0259
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-19 06:04:09 -0700

    basic functions for markov_chain added
    taking notes

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 from ca_markov.cc_markov import MarkovChain
-import fetch_data
+
+mc = MarkovChain()
+
+mc.add_file('dalecooper_scrapy/quotes.json')
+
+mc.generate_text()
+
+# convert json to text file, use function to load and generate text
