2dbfb71ca9264beb7c02e5639b5e707dafaa652e
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-19 12:13:32 -0700

    creating function in fetch_data.py for use in run,py

--- fetch_data.py
+++ fetch_data.py
@@ -2,24 +2,17 @@ from ca_markov.cc_markov import MarkovChain
 import csv
 import re
 
-quote_list = ""
+#reading scrapy item CSV file and writing into strings
 
-with open('dalecooper_scrapy/items.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        row = str(row)
-        quote = re.sub(r'[\[\]\"\']', '', row)
-        if quote == "quote":
-            del quote
-        else:
-            quote_list += quote + "\n"
-    print quote_list
-
-#with open('quotes.txt', 'w') as output_file:
-#        output_file.write(row)
-
-#mc = MarkovChain()
-
-#quotes = mc.add_file('quotes.txt')
-
-#mc.generate_text()
+def get_quote_list():
+    quote_list = ""
+    with open('dalecooper_scrapy/items.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            row = str(row)
+            quote = re.sub(r'[\[\]\"\']', '', row)
+            if quote == "quote":
+                del quote
+            else:
+                quote_list += quote + "\n"
+        return quote_list
--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 from ca_markov.cc_markov import MarkovChain
+import fetch_data
+
+#use function from fetch_data to grab CSV file and turn into strings
+
+quote_list = fetch_data.get_quote_list()
+
+print quote_list
