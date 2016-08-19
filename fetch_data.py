138400682ee427d4913b6330f488b02603a2361b
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-19 09:24:44 -0700

    loading JSON file and dumping into txt file

--- fetch_data.py
+++ fetch_data.py
@@ -1,2 +1,14 @@
 from ca_markov.cc_markov import MarkovChain
-import scrapy
+import json
+
+with open('dalecooper_scrapy/quotes.json') as data_file:
+    json_file = json.load(data_file)
+
+with open('quotes.txt', 'w') as outfile:
+    json.dump(json_file, outfile)
+
+#mc = MarkovChain()
+
+#quotes = mc.add_file('quotes.txt')
+
+#mc.generate_text()
--- run.py
+++ run.py
@@ -1,9 +1,1 @@
 from ca_markov.cc_markov import MarkovChain
-
-mc = MarkovChain()
-
-mc.add_file('dalecooper_scrapy/quotes.json')
-
-mc.generate_text()
-
-# convert json to text file, use function to load and generate text
