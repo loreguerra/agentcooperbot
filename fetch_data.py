7a318a9955944bbe90160058db2f3ed1f236fd28
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-19 11:58:49 -0700

    add if statement to delete CSV column header 'quote'

--- fetch_data.py
+++ fetch_data.py
@@ -9,16 +9,14 @@ with open('dalecooper_scrapy/items.csv', 'rb') as f:
     for row in reader:
         row = str(row)
         quote = re.sub(r'[\[\]\"\']', '', row)
-        quote_list += quote + "\n"
+        if quote == "quote":
+            del quote
+        else:
+            quote_list += quote + "\n"
     print quote_list
 
 #with open('quotes.txt', 'w') as output_file:
 #        output_file.write(row)
-
-
-
-#with open('quotes.txt', 'w') as outfile:
-#    json.dump(json_file, outfile)
 
 #mc = MarkovChain()
 
