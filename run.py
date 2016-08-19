3777748371b548057b65b4afa3e53d77bb56ef53
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-19 12:25:50 -0700

    creating instance of MarkovChain to use strings
    notes on what to do next

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 from ca_markov.cc_markov import MarkovChain
 import fetch_data
 
-#use function from fetch_data to grab CSV file and turn into strings
+#instance of MarkovChain
+mc = MarkovChain()
 
+#use function from fetch_data to grab CSV file and turn into strings
 quote_list = fetch_data.get_quote_list()
 
-print quote_list
+#Adding string to use to instance and generating text from instance
+MarkovChain.add_string(mc, quote_list)
+MarkovChain.generate_text(mc)
+
+#create function to call generate_text in reply
+#join list of words into string
+#display string as "Special Agent Dale Cooper: {string}"
