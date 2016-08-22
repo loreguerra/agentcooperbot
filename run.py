ff3a54e4ace327f7cd9e7e793c5c9b2c63a61fc6
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-22 09:09:59 -0700

    created start-up sequence and user reply function/nicknames

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from ca_markov.cc_markov import MarkovChain
 import fetch_data
 import filter_words
+from time import sleep
 
 #instance of MarkovChain
 mc = MarkovChain()
@@ -13,8 +14,22 @@ MarkovChain.add_string(mc, quote_list)
 
 # display loading... connecting... display status, type exit to exit, display hello from cooper
 # listen for input, generate response
+USER_NICK = ''
+DC_NICK = 'spagent_dc2'
 
+print 'Connecting...'
+sleep(2)
+print 'Loading...'
+sleep(2)
+print 'Connected. You are now online with spagent_dc2. Type "exit" to exit anytime. Specify your nickname with /nick.\n\n'
+print '%s: Special Agent Dale Cooper here.\n' % DC_NICK
+USER_NICK = raw_input('/nick:')
 
+# format user input
+def user_input():
+    user_reply = raw_input('%s: ' % USER_NICK)
+    if user_reply == 'exit':
+        quit()
 
 # Filter out weird sentence endings, capitalize first word and names
 def create_reply():
@@ -30,7 +45,11 @@ def create_reply():
             del markov_text[len(markov_text)-1]
         else:
             markov_filtered.append(word)
-    sentence = 'Special Agent Dale Cooper: %s.' % (' '.join(markov_filtered))
+    sentence = '%s: %s.' % (DC_NICK, ' '.join(markov_filtered))
     print sentence
 
 #create function to call generate_text in reply
+while True:
+    user_input()
+    sleep(3)
+    create_reply()
